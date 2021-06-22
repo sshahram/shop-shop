ad460fa9d1b20e8be4d40833577b8bc4d60e0af6
sshahram <shirin.shahram23@gmail.com>
2021-06-22 15:49:35 -0700

    create layout for shipping cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -9,6 +9,8 @@ import spinner from '../assets/spinner.gif';
 import { useStoreContext } from '../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../utils/actions';
 
+import Cart from '../components/Cart';
+
 function Detail() {
   // const { id } = useParams();
 
@@ -67,6 +69,7 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
+      <Cart />
     </>
   );
 }
