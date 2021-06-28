eb18875e66ec32a62ceeff851ffbc3ef7533266c
sshahram <shirin.shahram23@gmail.com>
2021-06-28 16:30:28 -0700

    add Redux to ProductList

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -5,8 +5,7 @@ import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 
-// import necessary action and context Hook functionality
-import { useStoreContext } from '../../utils/GlobalState';
+import { useSelector, useDispatch } from 'react-redux';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 
 import { idbPromise } from "../../utils/helpers";
@@ -26,7 +25,8 @@ function ProductList() {
   //   );
   // }
 
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
 
   const { currentCategory } = state;
 
