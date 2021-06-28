b01b3d2059eea0c5c3116116011973f0588d5bc8
sshahram <shirin.shahram23@gmail.com>
2021-06-28 15:38:05 -0700

    define initial state and pass it to the reducer

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useProductReducer } from './reducers';
+import reducer from './reducers';
 import { createStore } from 'redux';
 
 // create store object
-const store = createStore(useProductReducer);
+const store = createStore(reducer);
 
 export default store;
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -10,9 +10,16 @@ import {
     TOGGLE_CART
 } from "./actions";
 
-import { useReducer } from "react";
+// define initial state
+const initialState = {
+    products: [],
+    cart: [],
+    cartOpen: false,
+    categories: [],
+    currentCategory: '',
+};
 
-export const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
         case UPDATE_PRODUCTS:
@@ -80,6 +87,4 @@ export const reducer = (state, action) => {
     }
 };
 
-export function useProductReducer(initialState) {
-    return useReducer(reducer, initialState);
-}
+export default reducer;
